9eb4acfe9ec49926a4ff700cdcb2c294d10920ad
J Sai Manideepak <110416334+Manideepak2701@users.noreply.github.com>
2024-03-28 16:38:16 +0530

    Create Header.jsx

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    const logo = "https://voicemaker.in/img/logo.svg"; // Replace with your image URL
+    return (
+      <img src={logo} alt="logo" style={{height : 42}}/>
+    );
+  };
+  
+export default Header;
